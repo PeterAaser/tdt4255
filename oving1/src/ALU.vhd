02a036719db3187dba68c9ae256560cd10cc3645
Anders Lima <anders92lima@yahoo.no>
2015-10-10 16:38:48 +0200

    Add ALU module

---
+++ oving1/src/ALU.vhd
@@ -0,0 +1,98 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity ALU is
+    Port ( clk: in STD_LOGIC;
+			  read_data1 : in  std_logic_vector (31 downto 0);
+           read_data2 : in  std_logic_vector (31 downto 0);
+           instruction : in  std_logic_vector (5 downto 0);
+           ALUOp : in  std_logic_vector (1 downto 0);
+           Zero : out  std_logic;
+           ALUResult : inout  std_logic_vector (31 downto 0);
+           ALUSrc : in  std_logic);
+end ALU;
+
+architecture Behavioral of ALU is
+		type Operation_t is (ALU_ADD, ALU_SUB, ALU_SLT, ALU_AND, ALU_OR, ALU_A, ALU_B);
+		signal operation: Operation_t := ALU_ADD;
+		signal operatorA: std_logic_vector (31 downto 0);
+		signal operatorB: std_logic_vector (31 downto 0);
+		signal func: std_logic_vector ( 5 downto 0);
+		--signal immediate: std_logic_vector (15 downto 0);
+begin
+	mux : process(ALUSrc)
+	begin
+		if ALUSrc = '0' then
+			operatorB <= read_data2;
+		else
+			operatorB <= std_logic_vector(resize(signed(instruction(15 downto 0)), operatorB'length));
+		end if;
+	end process;
+
+	alu_control: process(clk, ALUOp, instruction)
+	begin
+		case ALUOp is
+			when "00"=> --R-type
+				case instruction is
+					when b"100000"=>
+						operation <= ALU_ADD;
+					when b"100100"=>
+						operation <= ALU_AND;
+					when b"100101"=>
+						operation <= ALU_OR;
+					when b"101010"=>
+						operation <= ALU_SLT;
+					when b"100010"=>
+						operation <= ALU_SUB;
+					when others=>
+						null;
+				end case;
+			when "01"=> --I-Type
+				operation <= ALU_B;
+			when others=>
+				null;
+		end case;
+	end process;
+		
+	alu_perform_op: process(clk)
+	begin
+		if rising_edge(clk) then
+			case operation is
+				when ALU_ADD=>
+					ALUResult <= std_logic_vector(signed(operatorA) + signed(operatorB));
+				when ALU_SUB=>
+					ALUResult <= std_logic_vector(signed(operatorA) - signed(operatorB));
+				when ALU_SLT=>
+					if operatorA < operatorB then
+						ALUResult <= x"00000001";
+					else
+						ALUResult <= x"00000000";
+					end if;
+				when ALU_AND=>
+					ALUResult <= operatorA and operatorB;
+				when ALU_OR=>
+					ALUResult <= operatorA or operatorB;
+				when ALU_A=>
+					ALUResult <= operatorA;
+				when ALU_B=>
+					ALUResult <= operatorB;
+				when others=>
+					null;
+			end case;
+		end if;
+	end process;
+	
+	alu_zero: process(ALUResult)
+	begin
+		if ALUResult = x"0000" then
+			Zero <= '1';
+		else
+			Zero <= '0';
+		end if;
+	end process;
+
+	operatorA <= read_data1;
+
+end Behavioral;
+
---
+++ oving1/src/tb_ALU.vhd
@@ -0,0 +1,88 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+ 
+ENTITY tb_ALU IS
+END tb_ALU;
+ 
+ARCHITECTURE behavior OF tb_ALU IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT ALU
+    PORT(
+         clk : IN  std_logic;
+         read_data1 : IN  std_logic_vector(31 downto 0);
+         read_data2 : IN  std_logic_vector(31 downto 0);
+         instruction : IN  std_logic_vector(5 downto 0);
+         ALUOp : IN  std_logic_vector(1 downto 0);
+         Zero : OUT  std_logic;
+         ALUResult : OUT  std_logic_vector(31 downto 0);
+         ALUSrc : IN  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal read_data1 : std_logic_vector(31 downto 0) := (others => '0');
+   signal read_data2 : std_logic_vector(31 downto 0) := (others => '0');
+   signal instruction : std_logic_vector(5 downto 0) := (others => '0');
+   signal ALUOp : std_logic_vector(1 downto 0) := (others => '0');
+   signal ALUSrc : std_logic := '0';
+
+ 	--Outputs
+   signal Zero : std_logic;
+   signal ALUResult : std_logic_vector(31 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: entity work.ALU PORT MAP (
+          clk => clk,
+          read_data1 => read_data1,
+          read_data2 => read_data2,
+          instruction => instruction,
+          ALUOp => ALUOp,
+          Zero => Zero,
+          ALUResult => ALUResult,
+          ALUSrc => ALUSrc
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+      wait for clk_period*10;
+
+      -- insert stimulus here 
+		read_data1 <= x"00000002";
+		read_data2 <= x"00000003";
+		ALUOp <= "00";
+		Instruction <= b"100000";
+		
+		wait for clk_period*5;
+		
+		assert Zero = '0';
+		assert ALUResult = x"00000005";
+		
+		
+      wait;
+   end process;
+
+END;
