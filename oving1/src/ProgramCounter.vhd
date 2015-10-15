c7b3673e675d8ea6296965fc02bd8f013caa634c
Øyvind Robertsen <oyvind@arcticstyle.net>
2015-10-15 12:57:45 +0200

    test program counter properly

--- oving1/src/ProgramCounter.vhd
+++ oving1/src/ProgramCounter.vhd
@@ -5,54 +5,41 @@ use work.Defs.all;
 
 entity ProgramCounter is
     generic (
-        ADDR_WIDTH : integer := 8;
-		  INSTR_WIDTH : integer := 26
+        ADDR_WIDTH : integer := 8
     );
     port (
         clk, reset          : in std_logic;
         pc_write            : in std_logic;
-		  jump					 : in std_logic;
-		  branch					 : in std_logic; 
-		  zero					 : in std_logic;
-        instruction			 : in std_logic_vector(INSTR_WIDTH - 1 downto 0);
+        jump                : in std_logic;
+        branch              : in std_logic; 
+        zero                : in std_logic;
+        instruction         : in std_logic_vector(26 - 1 downto 0);
         address_out         : out std_logic_vector(ADDR_WIDTH - 1 downto 0)
-		  --dbg_extended			 : out std_logic_vector(INSTR_WIDTH - 1 downto 0);
-		  --dbg_extended_sig	 : out std_logic_vector(INSTR_WIDTH - 1 downto 0)
-
     );
 end ProgramCounter;
 
 architecture Behavioral of ProgramCounter is
     signal address : std_logic_vector(ADDR_WIDTH - 1 downto 0);
 begin
-
     update: process(clk, reset)
-	 	  variable extended : std_logic_vector(INSTR_WIDTH - 1 downto 0);
-		  variable jump_adr : std_logic_vector(INSTR_WIDTH - 1 downto 0);
     begin
-		  --extended := std_logic_vector(resize(signed(instruction(15 downto 0)), INSTR_WIDTH ));
-		  --dbg_extended_sig <= std_logic_vector(resize(signed(instruction(15 downto 0)), INSTR_WIDTH ));
-
-		  
         if reset = '1' then
             address <= (others => '0');
         elsif rising_edge(clk) and pc_write = '1' then
-			
+
             if jump = '1' then
-					address <= instruction(7 downto 0);
-				else
-					if branch = '1' and zero = '1' then
-						address <= std_logic_vector(unsigned(address) + 1 + unsigned(instruction(7 downto 0)));
-					else
-						address <= std_logic_vector(unsigned(address) + 1);
-					end if;
+                address <= instruction(7 downto 0);
+            else
+                if branch = '1' and zero = '1' then
+                    address <= std_logic_vector(unsigned(address) + 1 + unsigned(instruction(7 downto 0)));
+                else
+                    address <= std_logic_vector(unsigned(address) + 1);
+                end if;
             end if;
         end if;
-		  --dbg_extended_sig <= extended_sig;
-		  --dbg_extended <= extended;
     end process;
 
     address_out <= address;
-    
+
 end Behavioral;
 
--- oving1/testbench/tb_ProgramCounter.vhd
+++ oving1/testbench/tb_ProgramCounter.vhd
@@ -15,6 +15,10 @@ ARCHITECTURE behavior OF tb_ProgramCounter IS
     signal clk : std_logic := '0';
     signal reset : std_logic := '0';
     signal pc_write : std_logic := '0';
+    signal jump : std_logic := '0';
+    signal branch : std_logic := '0';
+    signal zero : std_logic := '0';
+    signal instruction : std_logic_vector(25 downto 0) := "00000000000000000000000000";
 
  	--Outputs
     signal address_out : std_logic_vector(7 downto 0);
@@ -29,6 +33,10 @@ BEGIN
         clk => clk,
         reset => reset,
         pc_write => pc_write,
+        jump => jump,
+        branch => branch,
+        zero => zero,
+        instruction => instruction,
         address_out => address_out
     );
 
@@ -46,15 +54,31 @@ BEGIN
     stim_proc: process
     begin		
         -- hold reset state for 100 ns.
+        wait for clk_period / 2;
         reset <= '1';
-        wait for clk_period*5;	
+        wait for clk_period;	
         reset <= '0';
         assert address_out = x"00";
+        -- Regular increment
         pc_write <= '1';
         wait for clk_period;
-        pc_write <= '0';
         assert address_out = x"01";
-
+        jump <= '1';
+        instruction <= "00000000000000000010000000";
+        wait for clk_period;
+        -- JMP
+        assert address_out = x"80";
+        jump <= '0';
+        branch <= '1';
+        zero <= '1';
+        instruction <= "00000000000000000001000000";
+        wait for clk_period;
+        -- Branch
+        assert address_out = x"c1";
+        pc_write <= '0';
+        jump <= '0';
+        branch <= '0';
+        zero <= '0';
         -- insert stimulus here 
         wait;
    end process;
