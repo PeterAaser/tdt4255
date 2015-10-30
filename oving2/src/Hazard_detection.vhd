c2539495a7124eb24336e1cc0ab3ffcce0d7ca2c
peteraaser@gmail.com <peteraaser@gmail.com>
2015-10-30 13:15:05 +0100

    Add empty hazard detection unit

---
+++ oving2/src/Hazard_detection.vhd
@@ -0,0 +1,16 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all
+
+entity Hazard_detection is
+
+end Hazard_detection;
+
+architecture Behavioral of Hazard_detection is
+
+begin
+
+
+end Behavioral;
+
