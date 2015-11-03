7f12dc37c9559064fd89df176da5ac784974ebac
Anders Lima <anders92lima@yahoo.no>
2015-11-03 13:38:48 +0100

    Fix ALU muxing

--- oving2/src/ALU.vhd
+++ oving2/src/ALU.vhd
@@ -9,10 +9,10 @@ entity ALU is
            extended_immediate       : in std_logic_vector(31 downto 0);
            funct                    : in funct_t;       -- this is a bit vector whose function must be looked up
            
-           forward_a                : in forward_t;
-           forward_b                : in forward_t;
-           wb_alu_result_in            : in std_logic_vector(31 downto 0);
-           mem_alu_result_in           : in std_logic_vector(31 downto 0);
+           forward_a                : in forward_t := REG; -- control signal for forwarding muxes
+           forward_b                : in forward_t := REG;
+           wb_alu_result            : in std_logic_vector(31 downto 0) := (others => '0'); -- forwarded values
+           mem_alu_result           : in std_logic_vector(31 downto 0) := (others => '0');
            
            op                       : in op_t;          -- on the other hand this is an enumerated type.
            ALU_source               : in ALU_source_t;
@@ -29,7 +29,6 @@ architecture Behavioral of ALU is
     signal s_operandA:  signed (31 downto 0);
     signal s_operandB:  signed (31 downto 0);
     signal s_result:    signed (31 downto 0) := (others => '0');
-    --signal temp_ex_read: std_logic_vector(31 downto 0) := (others => '0');
 begin
 
 	alu_control: process(op, extended_immediate)	
@@ -41,37 +40,40 @@ begin
         end if;
     end process;
 
-	mux: process(forward_a, forward_b, read_data_1, read_data_2, ALU_source, mem_alu_result_in, wb_alu_result_in)	
-        --variable temp_ex_read: std_logic_vector(31 downto 0);
+	mux: process(forward_a, forward_b, read_data_1, read_data_2, ALU_source, mem_alu_result, wb_alu_result, extended_immediate)	
+        variable temp_ex_read: std_logic_vector(31 downto 0);
     begin
         
         case forward_a is
             when REG =>
                 s_operandA <= signed(read_data_1);
             when WB =>
-                s_operandA <= signed(wb_alu_result_in);
+                s_operandA <= signed(wb_alu_result);
             when MEM =>
-                s_operandA <= signed(mem_alu_result_in);
+                s_operandA <= signed(mem_alu_result);
             when others =>
                 null;
         end case;
         
---        case forward_b is
---            when REG =>
---                ex_read_data_2_forwarded <= read_data_2;
---            when WB =>
---                ex_read_data_2_forwarded <= wb_alu_result;
---            when MEM =>
---                ex_read_data_2_forwarded <= mem_alu_result;
---            when others =>
---                null;
---        end case;
---
---        if ALU_source = REG2 then
---            s_operandB <= signed(temp_ex_read);
---        else
---            s_operandB <= signed(extended_immediate);
---        end if;
+        case forward_b is
+            when REG =>
+                ex_read_data_2_forwarded <= read_data_2;
+                temp_ex_read := read_data_2;
+            when WB =>
+                ex_read_data_2_forwarded <= wb_alu_result;
+                temp_ex_read := wb_alu_result;
+            when MEM =>
+                ex_read_data_2_forwarded <= mem_alu_result;
+                temp_ex_read := mem_alu_result;
+            when others =>
+                null;
+        end case;
+
+        if ALU_source = REG2 then
+            s_operandB <= signed(temp_ex_read);
+        else
+            s_operandB <= signed(extended_immediate);
+        end if;
         
 	end process;
 
