a369c5c3ee8991ece6c308a76926089d3614aae0
Øyvind Robertsen <oyvind@arcticstyle.net>
2015-11-11 16:50:29 +0100

    test id-stage forwarding in testbench

--- oving2/testbench/tb_Forwarding.vhd
+++ oving2/testbench/tb_Forwarding.vhd
@@ -15,13 +15,17 @@ ARCHITECTURE behavior OF tb_Forwarding IS
     --Inputs
     signal mem_regd : reg_t;
     signal wb_regd : reg_t;
+    signal id_regs : reg_t;
+    signal id_regt : reg_t;
     signal ex_regs : reg_t;
     signal ex_regt : reg_t;
     signal mem_regwrite : RegWrite_t;
     signal wb_regwrite : RegWrite_t;
  	--Outputs
-    signal forward_a : Forward_t;
-    signal forward_b : Forward_t;
+    signal forward_id_a : Forward_t;
+    signal forward_id_b : Forward_t;
+    signal forward_ex_a : Forward_t;
+    signal forward_ex_b : Forward_t;
 
     -- Clock period definitions
     constant clk_period : time := 10 ns;
@@ -32,12 +36,16 @@ BEGIN
     uut: entity work.Forwarding port map(
         mem_regd => mem_regd,
         wb_regd => wb_regd,
+        id_regs => id_regs,
+        id_regt => id_regt,
         ex_regs => ex_regs,
         ex_regt => ex_regt,
         mem_regwrite => mem_regwrite,
         wb_regwrite => wb_regwrite,
-        forward_a => forward_a,
-        forward_b => forward_b
+        forward_id_a => forward_id_a,
+        forward_id_b => forward_id_b,
+        forward_ex_a => forward_ex_a,
+        forward_ex_b => forward_ex_b
     );
 
     -- Clock process definitions
@@ -63,8 +71,8 @@ BEGIN
         mem_regwrite <= false;
         wb_regwrite <= false;
         wait for clk_period/2;
-        assert forward_a = REG;
-        assert forward_b = REG;
+        assert forward_ex_a = REG;
+        assert forward_ex_b = REG;
         wait for clk_period/2;
         -- No dependency, mem_regwrite
         mem_regd <= "00001";
@@ -74,8 +82,8 @@ BEGIN
         mem_regwrite <= true;
         wb_regwrite <= false;
         wait for clk_period/2;
-        assert forward_a = REG;
-        assert forward_b = REG;
+        assert forward_ex_a = REG;
+        assert forward_ex_b = REG;
         wait for clk_period/2;
         -- No dependency, wb_regwrite
         mem_regd <= "00001";
@@ -85,8 +93,8 @@ BEGIN
         mem_regwrite <= false;
         wb_regwrite <= true;
         wait for clk_period/2;
-        assert forward_a = REG;
-        assert forward_b = REG;
+        assert forward_ex_a = REG;
+        assert forward_ex_b = REG;
         wait for clk_period/2;
         -- No dependency, both regwrites
         mem_regd <= "00001";
@@ -96,8 +104,8 @@ BEGIN
         mem_regwrite <= true;
         wb_regwrite <= true;
         wait for clk_period/2;
-        assert forward_a = REG;
-        assert forward_b = REG;
+        assert forward_ex_a = REG;
+        assert forward_ex_b = REG;
         wait for clk_period/2;
         -- ex_regs = wb_regd, no regwrite
         mem_regd <= "00001";
@@ -107,8 +115,8 @@ BEGIN
         mem_regwrite <= false;
         wb_regwrite <= false;
         wait for clk_period/2;
-        assert forward_a = REG;
-        assert forward_b = REG;
+        assert forward_ex_a = REG;
+        assert forward_ex_b = REG;
         wait for clk_period/2;
         -- ex_regs = wb_regd, wb_regwrite
         mem_regd <= "00001";
@@ -118,8 +126,8 @@ BEGIN
         mem_regwrite <= false;
         wb_regwrite <= true;
         wait for clk_period/2;
-        assert forward_a = WB;
-        assert forward_b = REG;
+        assert forward_ex_a = WB;
+        assert forward_ex_b = REG;
         wait for clk_period/2;
         -- ex_regs = ex_regt = wb_regd, wb_regwrite
         mem_regd <= "00001";
@@ -129,8 +137,8 @@ BEGIN
         mem_regwrite <= false;
         wb_regwrite <= true;
         wait for clk_period/2;
-        assert forward_a = WB;
-        assert forward_b = WB;
+        assert forward_ex_a = WB;
+        assert forward_ex_b = WB;
         wait for clk_period/2;
         -- ex_regs = wb_regd, ex_regt = mem_regd,
         -- wb_regwrite, mem_regwrite
@@ -141,8 +149,8 @@ BEGIN
         mem_regwrite <= true;
         wb_regwrite <= true;
         wait for clk_period/2;
-        assert forward_a = WB;
-        assert forward_b = MEM;
+        assert forward_ex_a = WB;
+        assert forward_ex_b = MEM;
         wait for clk_period/2;
         -- ex_regs = wb_regd, ex_regt = mem_regd,
         -- mem_regwrite
@@ -153,8 +161,8 @@ BEGIN
         mem_regwrite <= true;
         wb_regwrite <= false;
         wait for clk_period/2;
-        assert forward_a = REG;
-        assert forward_b = MEM;
+        assert forward_ex_a = REG;
+        assert forward_ex_b = MEM;
         wait for clk_period/2;
         -- ex_regs = wb_regd, ex_regt = mem_regd,
         -- wb_regwrite
@@ -165,8 +173,8 @@ BEGIN
         mem_regwrite <= false;
         wb_regwrite <= true;
         wait for clk_period/2;
-        assert forward_a = WB;
-        assert forward_b = REG;
+        assert forward_ex_a = WB;
+        assert forward_ex_b = REG;
         wait for clk_period/2;
         -- ex_regs = ex_regt = wb_regd
         -- wb_regwrite, mem_regwrite
@@ -177,8 +185,8 @@ BEGIN
         mem_regwrite <= true;
         wb_regwrite <= true;
         wait for clk_period/2;
-        assert forward_a = MEM;
-        assert forward_b = MEM;
+        assert forward_ex_a = MEM;
+        assert forward_ex_b = MEM;
         wait for clk_period/2;
         -- ex_regs = mem_regd = wb_regd
         -- ex_regt = mem_regd = wb_regd
@@ -190,8 +198,26 @@ BEGIN
         mem_regwrite <= true;
         wb_regwrite <= true;
         wait for clk_period/2;
-        assert forward_a = MEM;
-        assert forward_b = MEM;
+        assert forward_ex_a = MEM;
+        assert forward_ex_b = MEM;
+        wait for clk_period/2;
+        -- ex_regs = mem_regd = wb_regd
+        -- ex_regt = mem_regd = wb_regd
+        -- id_regs = wb_regd
+        -- wb_regwrite, mem_regwrite
+        mem_regd <= "00001";
+        wb_regd <= "00001";
+        ex_regs <= "00001";
+        ex_regt <= "00001";
+        id_regs <= "00001";
+        id_regt <= "00010";
+        mem_regwrite <= true;
+        wb_regwrite <= true;
+        wait for clk_period/2;
+        assert forward_ex_a = MEM;
+        assert forward_ex_b = MEM;
+        assert forward_id_a = WB;
+        assert forward_id_b = REG;
         wait for clk_period/2;
         wait;
    end process;
